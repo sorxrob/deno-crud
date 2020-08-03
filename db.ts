a6f569cd5835f53de28e99d440328f075c117c1e
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-08-03 11:50:18 +0800

    Update env settings

--- db.ts
+++ db.ts
@@ -1,13 +1,13 @@
 import { MongoClient, Database } from "https://deno.land/x/mongo@v0.9.1/mod.ts";
-import { config } from "https://deno.land/x/dotenv/mod.ts";
+// import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-console.log(config({export: true}));
+import "https://deno.land/x/dotenv/load.ts";
 
-const { DB_URI, DB_NAME } = config({ export: true });
+// const { DB_URI, DB_NAME } = config({ export: true });
 
 const client = new MongoClient();
-client.connectWithUri(DB_URI);
+client.connectWithUri(Deno.env.get('DB_URI') || '');
 
-const db = client.database(DB_NAME);
+const db = client.database(Deno.env.get('DB_NAME') || '');
 
 export { db };
