1c1cc40b6deb5d8d1369a944137088505663e500
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-08-03 11:40:02 +0800

    [refactor] Add export option to env

--- db.ts
+++ db.ts
@@ -1,11 +1,13 @@
 import { MongoClient, Database } from "https://deno.land/x/mongo@v0.9.1/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 
-console.log(config());
+console.log(config({export: true}));
+
+const { DB_URI, DB_NAME } = config({ export: true });
 
 const client = new MongoClient();
-client.connectWithUri(config().DB_URI);
+client.connectWithUri(DB_URI);
 
-const db = client.database(config().DB_NAME);
+const db = client.database(DB_NAME);
 
 export { db };
