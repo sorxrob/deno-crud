a83898b27a333b4f0d5a1d01a151ffd28c209d59
Robert Anthonie Soriano <sorianorobertc@gmail.com>
2020-08-03 11:52:35 +0800

    [refactor] Remove unused imports

--- db.ts
+++ db.ts
@@ -1,9 +1,6 @@
 import { MongoClient, Database } from "https://deno.land/x/mongo@v0.9.1/mod.ts";
-// import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 import "https://deno.land/x/dotenv/load.ts";
-
-// const { DB_URI, DB_NAME } = config({ export: true });
 
 const client = new MongoClient();
 client.connectWithUri(Deno.env.get('DB_URI') || '');
